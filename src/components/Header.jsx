98bfa77fc80db149093a1afdb946bb6ad3e78d37
ravipandeydu <rp120896@gmail.com>
2022-09-10 20:22:23 +0530

    Portfolio Added

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div>
+        <h1>Hey 👋...</h1>
+        <h1>I'm Ravishankar Kumar </h1>
+        <h2>I am a full stack web developer</h2>
+        <h1>Welcome to my portfolio ! </h1>
+      </div>
+    </>
+  );
+};
+
+export default Header;
